4e642e1762a69ab15075f7339ac12276fe822926
Anupsimha <anupsimha2@gmail.com>
2025-01-31 22:23:47 +0530

    Worked on adding some more components like student feedback section  and about us section in the project EDUSITY... dated on 31/01/2025 (Friday)

--- Week_2_ReactJS/Edusity/src/App.jsx
+++ Week_2_ReactJS/Edusity/src/App.jsx
@@ -4,6 +4,8 @@ import Hero from './components/Hero/Hero.jsx'
 import Programs from './components/Programs/Programs.jsx'
 import Title from './components/Title/Title.jsx'
 import About from './components/About/About.jsx'
+import Campus from './components/Campus/Campus.jsx'
+import Testimonials from './components/Testimonials/Testimonials.jsx'
 
 const App = () => {
   return (
@@ -14,6 +16,10 @@ const App = () => {
         <Title subtitle='Our PROGRAM' title='What we Offer'></Title>
         <Programs></Programs>
         <About></About>
+        <Title subtitle='Gallery' title='Campus Photos'></Title>
+        <Campus></Campus>
+        <Title subtitle='TESTIMONIALS' title='What STUDENT Says'></Title>
+        <Testimonials></Testimonials>
       </div>
     </div>
   )
