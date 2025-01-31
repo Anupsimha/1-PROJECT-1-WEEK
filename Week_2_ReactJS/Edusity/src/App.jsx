c939a60a7560a3ebf5cf2154973dacb429ace4f7
Anupsimha <anupsimha2@gmail.com>
2025-01-31 19:32:18 +0530

    Completed today's work of this commitment ONE_WEEK_ONE_PROJECT i.e dated on 31/01/25 (Friday)

--- Week_2_ReactJS/Edusity/src/App.jsx
+++ Week_2_ReactJS/Edusity/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
 import Hero from './components/Hero/Hero.jsx'
+import Programs from './components/Programs/Programs.jsx'
+import Title from './components/Title/Title.jsx'
+import About from './components/About/About.jsx'
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Hero></Hero>
+      <div className="container">
+        <Title subtitle='Our PROGRAM' title='What we Offer'></Title>
+        <Programs></Programs>
+        <About></About>
+      </div>
     </div>
   )
 }
