25eb6b2b09264b7d1de33a6066026d6332d791c2
Anupsimha <anupsimha2@gmail.com>
2025-02-02 20:00:41 +0530

    Finished The project of 2nd Week of My Commitment i.e ONE_PROJECT_ONE_WEEK...Dated on 2/2/25

--- Week_2_ReactJS/Edusity/src/App.jsx
+++ Week_2_ReactJS/Edusity/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
 import Hero from './components/Hero/Hero.jsx'
 import Programs from './components/Programs/Programs.jsx'
@@ -8,8 +8,12 @@ import Campus from './components/Campus/Campus.jsx'
 import Testimonials from './components/Testimonials/Testimonials.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import Footer from './components/Footer/Footer.jsx'
+// import VideoPlayer from './components/VideoPlayer/VideoPlayer.jsx'
 
 const App = () => {
+
+  const [playState , setPlayState] = useState(false);
+
   return (
     <div>
       <Navbar></Navbar>
@@ -17,7 +21,7 @@ const App = () => {
       <div className="container">
         <Title subtitle='Our PROGRAM' title='What we Offer'></Title>
         <Programs></Programs>
-        <About></About>
+        <About setPlayState={setPlayState}></About>
         <Title subtitle='Gallery' title='Campus Photos'></Title>
         <Campus></Campus>
         <Title subtitle='TESTIMONIALS' title='What STUDENT Says'></Title>
@@ -26,6 +30,7 @@ const App = () => {
         <Contact></Contact>
         <Footer></Footer>
       </div>
+      {/* <VideoPlayer playState={playState} setPlayState={setPlayState}></VideoPlayer> */}
     </div>
   )
 }
--- Week_2_ReactJS/Edusity/src/components/Footer/Footer.jsx
+++ Week_2_ReactJS/Edusity/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-      <p>&copy 2025 Anupsimha. All rights reserved.</p>
+      <p>&copy; 2025 Anupsimha. All rights reserved.</p>
       <ul>
         <li>Terms of Service</li>
         <li>Provacy Policy</li>
