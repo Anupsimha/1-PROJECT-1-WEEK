9a677b50976293aba83981fd8f7c401e6eae48e6
Anupsimha <anupsimha2@gmail.com>
2025-02-04 00:15:14 +0530

    Started new project for Third week of this commitment 'Gemini AI Clone' on 3/3/25(Monday)

---
+++ Week_3_ReactJS/Gemini AI Clone/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Sidebar from './Components/Sidebar/Sidebar'
+
+const App = () => {
+  return (
+    <>
+      <Sidebar></Sidebar>
+    </>
+  )
+}
+
+export default App
