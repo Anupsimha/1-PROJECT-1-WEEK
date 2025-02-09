7101eba14cfc362502f0d901d8738ea9be2f7960
Anupsimha <anupsimha2@gmail.com>
2025-02-09 19:51:09 +0530

    So, Got onto this commitment after a very beatiful trip to Mahakumbh(Always grateful for my family for providing this oppurtunity. so, completed only the UI part of Gemini AI clone for 3rd week of my project in ReactJS. If possible i will Integrate the UI part in future....9/2/25 (Sunday)

--- Week_3_ReactJS/Gemini AI Clone/src/Context/Context.jsx
+++ Week_3_ReactJS/Gemini AI Clone/src/Context/Context.jsx
@@ -1,31 +1,31 @@
-import { createContext, useState } from "react";
-import runChat from "../config/gemini";
+// import { createContext, useState } from "react";
+// // import runChat from "../config/gemini";
 
-export const Context = createContext();
+// export const Context = createContext();
 
-const ContextProvider = (props) => {
+// const ContextProvider = (props) => {
 
-    const [input , setInput] = useState("");
-    const [recentPrompt , setRecentPrompt] = useState("");
-    const [prevPrompt , setPrevPrompt] = useState([]);
-    const [showResult , setShowResult] = useState(false);
-    const [loading , setLoading] = useState(false);
-    const [resultData , setResultData] = useState("");
+//     const [input , setInput] = useState("");
+//     const [recentPrompt , setRecentPrompt] = useState("");
+//     const [prevPrompt , setPrevPrompt] = useState([]);
+//     const [showResult , setShowResult] = useState(false);
+//     const [loading , setLoading] = useState(false);
+//     const [resultData , setResultData] = useState("");
 
-    const onSent = async (prompt) => {
-        await runChat(prompt)
-    }
+//     const onSent = async (prompt) => {
+//         await runChat(prompt)
+//     }
 
-    const contextValue = {
+//     const contextValue = {
         
-    }
+//     }
 
-    return (
-        <ContextProvider value={contextValue}>
-            {props.children}
-        </ContextProvider>
-    )
+//     return (
+//         <ContextProvider value={contextValue}>
+//             {props.children}
+//         </ContextProvider>
+//     )
 
-}
+// }
 
-export default ContextProvider
+// export default ContextProvider
