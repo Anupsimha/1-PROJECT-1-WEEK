a79809c539433e0e3851c789fc4d065366e01eb1
Anupsimha <anupsimha2@gmail.com>
2025-02-10 22:52:33 +0530

    Started new project of Week 4 'EV-olution'. Already completed 40% of this project. Took this so to revise React components....10/2/25(Monday)

---
+++ Week_4_ReactJS/EV-loution/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import Background from './Components/Background/Background';
+import Navbar from './Components/Navbar/Navbar';
+
+const App = () => {
+
+  let heroData = [
+    {text1 : "Dive into " , text2 : "what you love"},
+    {text1 : "Indulge" , text2 : "your passions"},
+    {text1 : "Give in to" , text2 : "your passions"},
+  ]
+
+  const [heroCount , setHeroCount] = useState(0);
+  const [playStatus , setPlayStatus] = useState(true);
+
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount}></Background>
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default App
