44ade04a64dd8744ef837a2247f198aa2b4af822
Anupsimha <anupsimha2@gmail.com>
2025-03-03 19:10:55 +0530

    Progressed some more in this project by completing the backend part....Moving towads frontend...3/3/25(Monday)

--- MERN_I_WEEK_6_7/backend/server.js
+++ MERN_I_WEEK_6_7/backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
 
 import authRoutes from './routes/authRoutes.js';
+import messageRoutes from './routes/messageRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -10,9 +13,11 @@ const PORT = 5000 || process.env.PORT;
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth" , authRoutes);
-
+app.use("/api/messages" , messageRoutes);
+app.use("/api/users" , userRoutes);
 
 
 app.listen(PORT , () => {
