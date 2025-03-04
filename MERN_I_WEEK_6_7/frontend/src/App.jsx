89e1362f894600b8ec1fecb01d0d1355b86ee044
Anupsimha <anupsimha2@gmail.com>
2025-03-05 00:07:19 +0530

    Did.nt progressed much today, but it was not like that i did'nt did anything towards this project....4/3/25(Tuesday)

--- MERN_I_WEEK_6_7/frontend/src/App.jsx
+++ MERN_I_WEEK_6_7/frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './App.css'
+import Login from './pages/login/Login'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <Login></Login>
     </div>
   )
 }
--- MERN_I_WEEK_6_7/frontend/tailwind.config.js
+++ MERN_I_WEEK_6_7/frontend/tailwind.config.js
@@ -1,11 +1,21 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")], // 👈 Add this line if missing
+};
+
