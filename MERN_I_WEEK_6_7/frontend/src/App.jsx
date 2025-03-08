b0884d2288051b73150c10ed0850bac1868f67e6
Anupsimha <anupsimha2@gmail.com>
2025-03-09 01:07:59 +0530

    Added UI interface for the project 'Chatify'....Need to complete atleast 80% of the project tommorow....8/3/25(Saturday)

--- MERN_I_WEEK_6_7/frontend/src/App.jsx
+++ MERN_I_WEEK_6_7/frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
-import Login from './pages/login/Login'
+// import Login from './pages/login/Login'
+// import SignUp from './pages/signup/SignUp'
+import Home from './pages/home/Home'
 
 const App = () => {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Login></Login>
+      <Home></Home>
     </div>
   )
 }
