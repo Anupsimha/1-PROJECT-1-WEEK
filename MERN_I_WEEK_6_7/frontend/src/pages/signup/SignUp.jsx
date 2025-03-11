92e25b87029a16a8809b8d7ab06f23be044e2267
Anupsimha <anupsimha2@gmail.com>
2025-03-11 19:11:17 +0530

    Added The login and signup functionality int this project...Need to complete this project of week 6 and 7 before going onto the week 8th and 9th...11/3/25(Tuesday)

--- MERN_I_WEEK_6_7/frontend/src/pages/signup/SignUp.jsx
+++ MERN_I_WEEK_6_7/frontend/src/pages/signup/SignUp.jsx
@@ -71,7 +71,9 @@ const SignUp = () => {
               <Link to="/login" className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>Already Have an account?</Link>
 
               <div>
-                <button className='btn btn-block btn-sm mt-2 border border-slate-700'>Sign Up</button>
+                <button className='btn btn-block btn-sm mt-2 border border-slate-700' disabled={loading}>
+                  {loading ? <span className='loading loading-spinner'></span> : "sign Up"}
+                </button>
               </div>
 
             </form>
