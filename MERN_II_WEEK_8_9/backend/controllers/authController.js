c1c48149e7091a36cbb24c5698f4bd1dafc4d2a5
Anupsimha <anupsimha2@gmail.com>
2025-03-15 01:11:44 +0530

    Started a new project 'Twitter Clone'...it also a done using a ref of a youtuber,, but gained a lot of knowledge....Might start creating my new own projects by end of 11th or 12th week...14/3/25(Friday)

---
+++ MERN_II_WEEK_8_9/backend/controllers/authController.js
@@ -0,0 +1,67 @@
+import bcrypt from 'bcryptjs';
+
+import User from '../models/userModel.js'
+import generateTokenAndSetCookie from '../lib/utils/generateToken.js';
+
+export const signup = async (req , res) => {
+    try {
+        
+        const { fullName , username , email , password } = req.body;
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            return res.status(400).json({ error : "Invalid email Format" });
+        }
+
+        const existingUser = await User.findOne({ username });
+        if(existingUser){
+            return res.status(400).json({ error : "Username is already taken." })
+        }
+
+        const existingEmail = await User.findOne({ email });
+        if(existingEmail){
+            return res.status(400).json({ error : "Email already exists" })
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password , salt);
+
+        const newUser = new User ({
+            fullName,
+            username,
+            email,
+            password:hashedPassword
+        })
+
+        if(newUser){
+            generateTokenAndSetCookie(newUser._id , res)
+            await newUser.save();
+
+            res.status(201).json({
+                _id : newUser._id,
+                fullName : newUser.fullName,
+                username : newUser.username,
+                email : newUser.email,
+                followers : newUser.followers,
+                following : newUser.following,
+                profileImg : newUser.profileImg,
+                coverImg : newUser.coverImg,
+            })
+        }
+        else{
+            res.status(400).json({ error : "Invalid user data"});
+        }
+
+    } catch (error) {
+        console.log("Error in signup Controller : " , error.message);
+        res.status(500).json({ error : "Internal Server Error"});
+    }
+}
+
+export const login = async (req , res) => {
+    
+}
+
+export const logout = async (req , res) => {
+    
+}
---
+++ MERN_II_WEEK_8_9/backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import authRoutes from './routes/authRoutes.js'
+import connectToMongoDB from './db/connectToMongoDB.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use('/api/auth' , authRoutes);
+
+app.listen(PORT , () => {
+    console.log(`Server is running on port ${PORT}`)
+    connectToMongoDB();
+})
