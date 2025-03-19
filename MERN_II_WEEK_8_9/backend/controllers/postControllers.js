d7a2882ddc8353d66e6f7cd4e7668d24a68425e3
Anupsimha <anupsimha2@gmail.com>
2025-03-19 23:57:30 +0530

    Finished 95% of the backend Part...19/3/25(Wednesday)

--- MERN_II_WEEK_8_9/backend/controllers/postControllers.js
+++ MERN_II_WEEK_8_9/backend/controllers/postControllers.js
@@ -151,4 +151,76 @@ export const getAllPosts = async (req, res) => {
     }
 }
 
-export const getLikedPosts = async (req, res) => {}
+export const getLikedPosts = async (req, res) => {
+
+    const userId = req.params.id;
+
+    try {
+        const user = await User.findById(userId);
+        if(!user)   return res.status(404).json({error : "User not found"});
+
+        const likedPosts = await Post.find({_id: {$in: user.likedPosts}})
+        .populate({
+            path : "user",
+            select : '-password'
+        })
+        .populate({
+            path : "comments.user",
+            select : "-password"
+        });
+
+        res.status(200).json(likedPosts)
+
+    } catch (error) {
+        console.log("Error in getLikedPosts controller : " , error.message);
+        res.status(500).json({error : 'Internal Server Error'});
+    }
+
+}
+
+export const getFollowingPosts = async (req , res) => {
+    try {
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+        if(!user) return res.status(404).json({error : "User not found"});
+        
+        const following = user.following;
+
+        const findPosts = await Post.find({ user : {$in : following }})
+        .sort({ createdAt : - 1 })
+        .populate({
+            path : "user",
+            select : "-password",
+        });
+
+        res.status(200).json(findPosts);
+    } catch (error) {
+        console.log("Error in getFollowingPosts controller : " , error.message);
+        res.status(500).json({error : "Internal server Error"})
+    }
+}
+
+export const getUserPosts = async (req , res) => {
+    try {
+        const {username} = req.params;
+
+        const user = await User.findOne({username});
+        if(!user) return res.status(400).json({error : "User not Found"});
+
+        const posts = await Post.find({user : user._id}).sort({createdAt : -1})
+        .populate({
+            path : "User",
+            select : "-password",
+        })
+        .populate({
+            path : "comments.user",
+            select : "-password",
+        });
+
+        res.status(200).json(posts);
+
+    } catch (error) {
+        console.log("Error in getUserPosts controller : " , error.message);
+        return res.status(500).json({error : "Internal Server Error"});
+    }
+}
--- MERN_II_WEEK_8_9/backend/routes/postRoutes.js
+++ MERN_II_WEEK_8_9/backend/routes/postRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
 import { protectRoute } from '../middleware/protectRoute.js';
-import { getAllPosts , getLikedPosts , createPost , deletePost , commentOnPost , likeUnlikePost } from '../controllers/postControllers.js';
+import { getAllPosts , getLikedPosts , createPost , deletePost , commentOnPost , likeUnlikePost , getFollowingPosts , getUserPosts } from '../controllers/postControllers.js';
 
 const router = express.Router();
 
 router.get('/all' , protectRoute , getAllPosts);
+router.get('/following' , protectRoute , getFollowingPosts);
 router.get('/likes/:id' , protectRoute , getLikedPosts);
+router.get('/user/:username' , protectRoute , getUserPosts);
 router.post('/create' , protectRoute , createPost);
 router.post('/like/:id' , protectRoute , likeUnlikePost);
 router.post('/comment/:id' , protectRoute , commentOnPost);
