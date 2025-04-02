633ff7bfa92ebd505ba4b1b011fb073da94f4348
Anupsimha <anupsimha2@gmail.com>
2025-04-03 00:02:32 +0530

    Finished 95% of Home.jsx..Need to add only one more section, will add it at the end of the project....2/4/2025(Wednesday)

--- MERN_III_WEEK_10_11_12/frontend/src/components/Home/Home.jsx
+++ MERN_III_WEEK_10_11_12/frontend/src/components/Home/Home.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 
 import { assets } from '../../assets/assets.js'
+import Button from '../UI/Button.jsx'
 
 const Home = () => {
   return (
-    <div className='w-full  flex flex-col justify-center items-center h-screen'>
-      <div className='w-[100%] justify-between bg-red-500 flex items-center '>
-        <div className='flex flex-col w-[65%] h-72'>
-          <p className=''>M.N. Anupsimha</p>
-          <p>UNIQ<span>FLARE.</span></p>
-          <div className='flex'>
-            <p>Software Developer</p>
-            <p>Web Developer</p>
-            <p>App Developer</p>
-            <p>UI/UX Designer</p>
+    <div className='w-[100%] -mt-11 flex flex-col justify-center items-center h-screen'>
+      <div className='flex items-center justify-center w-[97.5%]'>
+        <div className='flex flex-col tracking-widest w-[68%] h-72'>
+          <p className='text-5xl bg-gray-500/55 w-fit py-1.5 px-2 font-bold rounded-lg'>M.N. Anupsimha</p>
+          <p className='text-[128px] font-semibold  font-josefin'>UNIQ<span className='text-[#E79600] font-bold'>FLARE.</span></p>
+          <div className="w-[72%] grid grid-cols-2 gap-2 gap-x-[6px] -mt-4">
+            <p className="text-3xl text-left bg-gray-500/55 py-1.5 rounded-lg font-bold w-fit px-6">Web Developer</p>
+            <p className="text-3xl text-left bg-gray-500/55 py-1.5 rounded-lg font-bold w-fit px-6">App Developer</p>
+            <p className="text-3xl text-left bg-gray-500/55 py-1.5 rounded-lg font-bold w-fit px-6">UI/UX Designer</p>
+            <p className="text-3xl w-[114%] text-left bg-gray-500/55 py-1.5 rounded-lg font-bold px-6">Software Developer</p>
           </div>
         </div>
-        <div className='bg-black h-72 w-[35%] rounded-3xl flex justify-center items-center'>
+        <div className='bg-black h-80 w-[45%] rounded-3xl flex justify-center items-center'>
           <img className='h-96 w-96' src= {assets.heroImage} alt="" />
         </div>
+      </div>
+      <div>
+        <Button></Button>
+      </div>
+      <div className='absolute bottom-2 left-0 w-full flex justify-center items-center'>
+        <p>Copyright &copy; 2025 M N Anupsimha</p>
       </div>
     </div>
   )
