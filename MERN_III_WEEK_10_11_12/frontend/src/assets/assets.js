0bea10474cd247a08e1f07d0cc12e6e4e7487d83
Anupsimha <anupsimha2@gmail.com>
2025-04-04 19:36:49 +0530

    Finished About me section...4/4/2025(Friday)

--- MERN_III_WEEK_10_11_12/frontend/src/assets/assets.js
+++ MERN_III_WEEK_10_11_12/frontend/src/assets/assets.js
@@ -2,10 +2,18 @@ import logo from './Portfolio_logo.png'
 import heroImage from './heroImage.png'
 import education from './education.png'
 import experience from './experience.png'
+import appdev from './app_crafting.png'
+import webdev from './web_development.png'
+import uiux from './UIUX_Designing.png'
+import backend from './backend_dev.png'
 
 export const assets = {
     logo,
     heroImage,
     education,
     experience,
+    appdev,
+    webdev,
+    uiux,
+    backend,
 }
