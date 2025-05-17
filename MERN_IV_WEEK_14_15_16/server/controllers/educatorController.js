004733f5f08c55116a5c91d41434e4932e123647
Anupsimha <anupsimha2@gmail.com>
2025-05-17 22:02:03 +0530

    Worked on Courses schema and educatorController.js....17/5/2025(Saturday)

--- MERN_IV_WEEK_14_15_16/server/controllers/educatorController.js
+++ MERN_IV_WEEK_14_15_16/server/controllers/educatorController.js
@@ -1,4 +1,6 @@
 import { clerkClient } from '@clerk/express';
+import Course from '../models/Course.js';
+import {v2 as cloudinary} from 'cloudinary';
 
 //Update role to educator
 export const updateRoleEducator = async (req , res) => {
@@ -21,4 +23,36 @@ export const updateRoleEducator = async (req , res) => {
             message: error.message
         })
     }
+}
+
+// Add New Course
+
+export const addCourse = async (req , res) => {
+    try {
+        const {courseData} = req.body;
+        const imgFile = req.file;
+        const educatorId = req.auth.userId;
+        
+        if(!imgFile){
+            return res.json({success: false, message: 'Thumbnail not attached'})
+        }
+
+        const parsedCourseData = await JSON.parse(courseData);
+        parsedCourseData.educator = educatorId;
+        const newCourse = await Course.create(parsedCourseData);
+        const imgUpload = await cloudinary.uploader.upload(imgFile.path);
+        newCourse.courseThumbnail = imgUpload.secure_url
+        await newCourse.save();
+
+        res.json({success: true, message: 'Course Added'})
+
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
+
+// Get Educator Courses
+
+export const getEducatorCourses = async (req , res) => {
+    
 }
--- MERN_IV_WEEK_14_15_16/server/routes/educatorRoutes.js
+++ MERN_IV_WEEK_14_15_16/server/routes/educatorRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { updateRoleEducator } from '../controllers/educatorController.js';
+import { addCourse, updateRoleEducator } from '../controllers/educatorController.js';
+import { protectEducator } from '../midldlewares/authMiddleware.js';
 
 const educatorRouter = express.Router();
 
 //add Educator Role
 educatorRouter.get('/update-role' , updateRoleEducator);
+educatorRouter.post('/add-course' , upload.single('image') , protectEducator , addCourse);
 
 export default educatorRouter;
